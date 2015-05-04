6c15f25b4543f3e29a8bb2bde321418bd1613fb4
Kyle Campos <kyle.campos@gmail.com>
2015-05-04 14:24:02 -0700

    Add missing config params

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,10 @@ class google_auth_proxy::params {
   $filename         = "google_auth_proxy-${version}.linux-amd64.go1.4.2.tar.gz"
   $download_url     = "https://github.com/bitly/google_auth_proxy/releases/download/v${version}/${filename}"
 
+  $upstreams           = hiera('google_auth_upstreams', ['http://127.0.0.1:8080'])
   $google_apps_domains = hiera('google_apps_domains', [])
   $http_address        = hiera('google_auth_proxy_http_address', '127.0.0.1:4180')
-  $htpasswd_file       = hiera('oogle_auth_proxy_htpasswd_file', '')
+  $htpasswd_file       = hiera('google_auth_proxy_htpasswd_file', '')
+  $auth_emails_file    = hiera('google_auth_emails_file', '')
+  $cookie_secret       = hiera('google_auth_cookie_secret', undef)
 }
